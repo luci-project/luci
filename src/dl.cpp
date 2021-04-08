4f36d0f67291281608af3b0684ef90caf5b096f7
Bernhard Heinloth <heinloth@cs.fau.de>
2021-04-08 09:28:23 +0200

    fix trampoline

--- src/dl.cpp
+++ src/dl.cpp
@@ -11,10 +11,43 @@ extern "C" void * dl_resolve(const Object & o, size_t index) {
 // TODO: Save registers
 asm(R"(
 .globl _dl_resolve
+.hidden _dl_resolve
 .type _dl_resolve, @function
+.align 16
 _dl_resolve:
+	# Save base pointer
+	push %rbx
+	mov %rsp, %rbx
+
+	# Save register
+	push %rax
+	push %rcx
+	push %rdx
+	push %rsi
+	push %rdi
+	push %r8
+	push %r9
+
+	# Read parameter (pushed bei plt) from stack
+	mov 8(%rbx), %rdi
+	mov 16(%rbx), %rsi
+	# Call high level resolve function
+	call dl_resolve
+	# Store result (resolved function) in temporary register
+	mov %rax, %r11
+
+	# Restore register
+	pop %r9
+	pop %r8
 	pop %rdi
 	pop %rsi
-	call dl_resolve
-	jmp *%rax
+	pop %rdx
+	pop %rcx
+	pop %rax
+
+	# Adjust stack (remove %rbx and parameter pushed by plt)
+	add $24, %rsp
+
+	# Jump to resolved function
+	jmp *%r11
 )");
