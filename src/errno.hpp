04d8ae421149ebffe70c0964a59b1bc616a91c4d
Bernhard Heinloth <heinloth@cs.fau.de>
2021-05-28 11:49:42 +0200

    Errno (wip)

---
+++ src/errno.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdlib>
+#include <cerrno>
+
+#ifndef errno
+#define errno (*__errno_location())
+#endif
+
+extern "C" int *__errno_location();
+
+extern "C" char * strerror_r(int errnum, char *buf, size_t buflen);
+extern "C" char * strerror(int errnum);
