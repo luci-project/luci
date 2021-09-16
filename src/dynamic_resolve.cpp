e4d1a7a7a77322ef08f90e7dc8541b531e2f4e92
Bernhard Heinloth <heinloth@cs.fau.de>
2021-09-16 19:51:08 +0200

    Add call frame information in resolve ASM

--- src/dynamic_resolve.cpp
+++ src/dynamic_resolve.cpp
@@ -1,6 +1,6 @@
 #include "object/base.hpp"
 
-extern "C" __attribute__((__used__)) void * dlresolve(const Object & o, size_t index) {
+extern "C" __attribute__((__used__)) void * __dlresolve(const Object & o, size_t index) {
 #ifndef NO_FPU
 	const uint32_t mask_low = 0xff;  // assume XSAVE & AVX, TODO: Use CPUID
 	const uint32_t mask_high = 0;
@@ -21,40 +21,122 @@ asm(R"(
 .type _dlresolve, @function
 .align 16
 _dlresolve:
+	.cfi_startproc
+	# Two arguments (object & index) are passed by PLT
+	.cfi_adjust_cfa_offset 16
+	endbr64
+
 	# Save base pointer
 	push %rbp
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rbp, 0
 	mov %rsp, %rbp
 
 	# Save register
 	push %rax
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rax, 0
+	push %rbx
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rbx, 0
 	push %rcx
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rcx, 0
 	push %rdx
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rdx, 0
 	push %rsi
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rsi, 0
 	push %rdi
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset rdi, 0
 	push %r8
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r8, 0
 	push %r9
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r9, 0
+	push %r10
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r10, 0
+	push %r11
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r11, 0
+	push %r12
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r12, 0
+	push %r13
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r13, 0
+	push %r14
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r14, 0
+	push %r15
+	.cfi_adjust_cfa_offset 8
+	.cfi_rel_offset r15, 0
 
 	# Read parameter (pushed bei plt) from stack
 	mov 8(%rbp), %rdi
 	mov 16(%rbp), %rsi
 	# Call high level resolve function
-	call dlresolve
-	# Store result (resolved function) in temporary register
-	mov %rax, %r11
+	call __dlresolve
+	# Store result (resolved function) on stack (previously used by first parameter pushed by plt)
+	mov %rax, 16(%rbp)
 
 	# Restore register
+	pop %r15
+	.cfi_adjust_cfa_offset -8
+	pop %r14
+	.cfi_adjust_cfa_offset -8
+	pop %r13
+	.cfi_adjust_cfa_offset -8
+	pop %r12
+	.cfi_adjust_cfa_offset -8
+	pop %r11
+	.cfi_adjust_cfa_offset -8
+	pop %r10
+	.cfi_adjust_cfa_offset -8
 	pop %r9
+	.cfi_adjust_cfa_offset -8
 	pop %r8
+	.cfi_adjust_cfa_offset -8
 	pop %rdi
+	.cfi_adjust_cfa_offset -8
 	pop %rsi
+	.cfi_adjust_cfa_offset -8
 	pop %rdx
+	.cfi_adjust_cfa_offset -8
 	pop %rcx
+	.cfi_adjust_cfa_offset -8
+	pop %rbx
+	.cfi_adjust_cfa_offset -8
 	pop %rax
+	.cfi_adjust_cfa_offset -8
 	pop %rbp
+	.cfi_adjust_cfa_offset -8
 
-	# Adjust stack (remove parameter pushed by plt)
-	add $16, %rsp
+	.cfi_restore rbp
+	.cfi_restore rax
+	.cfi_restore rbx
+	.cfi_restore rcx
+	.cfi_restore rdx
+	.cfi_restore rsi
+	.cfi_restore rdi
+	.cfi_restore r8
+	.cfi_restore r9
+	.cfi_restore r10
+	.cfi_restore r11
+	.cfi_restore r12
+	.cfi_restore r13
+	.cfi_restore r14
+	.cfi_restore r15
+
+	# Adjust stack (remove second parameter pushed by plt)
+	add $8, %rsp
+	.cfi_adjust_cfa_offset -8
 
 	# Jump to resolved function
-	jmp *%r11
+	ret
+	.cfi_endproc
 )");
