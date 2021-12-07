24ae5ba2e58cc06d402cc5992f791a7fa7876fdc
Bernhard Heinloth <heinloth@cs.fau.de>
2021-12-07 10:43:10 +0100

    Uprobe: include returns, improve output

--- tools/uprobe.sh
+++ tools/uprobe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail 
+set -euo pipefail
 
 status=0
 prefix="luci"
@@ -11,18 +11,38 @@ if [[ ! -d "$tracing" ]] ; then
 elif [[ $# -eq 0 ]] ; then
 	echo "Usage:"
 	echo "	$0 [FILE] [[PID]]"
+	echo "	$0 func"
 	echo "	$0 show"
-	echo "	$0 detail"
 	echo "	$0 clear"
 	echo "	$0 stop"
+elif [[ "${1,,}" == "func" ]] ; then
+	cat $tracing/trace | sed -n -e 's/^[ ]*\([a-zA-Z0-9_-]\+\)-\([0-9]\+\) .*\.[0-9]\+: \([^:]\+\): (.*$/    \3 (\1 \/ PID \2) /p' | sort -u
 elif [[ "${1,,}" == "show" ]] ; then
-	cat $tracing/trace | sed -n -e 's/[ ]*\([a-zA-Z]\+\)-\([0-9]\+\) .*\.[0-9]\+: \([^:]\+\): (.*$/    \3 (\1 / PID \2) /p' | sort -u
-elif [[ "${1,,}" == "detail" ]] ; then
 	boottime=$(sed -n -e 's/btime //p' /proc/stat)
 	while IFS= read -r line ; do
 		eval $line  # Ugly, dangerous, but fuck it.
-		echo "$(date -d @$(( boottime + LOGSEC )) +"%Y-%m-%d %H:%M:%S").$LOGMILLIS $(sed "${MAPID}q;d" $helpmap) @ $LOGADDR in $LOGCOMM / PID $LOGPID (Core $(( LOGCORE + 0 ))) from $RET"
-	done < <(cat $tracing/trace | sed -n -e 's/[ ]*\([a-zA-Z]\+\)-\([0-9]\+\)[ ]\+\[\([0-9]\+\)\] .... \([0-9]\+\).\([0-9]\+\): \([^:]\+\): (\(0x[0-9a-f]\+\))/LOGCOMM="\1" LOGPID=\2 LOGCORE="\3" LOGSEC=\4 LOGMILLIS=\5 LOGSYM="\6" LOGADDR="\7"/p')
+		if [[ "$LOGADDR" == *" <- "* ]] ; then
+			RETADDR=${LOGADDR%% <- *}
+			LOGADDR=${LOGADDR##* <- }
+		fi
+		mapdata=$(sed "${MAPID}q;d" $helpmap)
+		echo -n "$(date -d @$(( boottime + LOGSEC )) +"%Y-%m-%d %H:%M:%S").$LOGMILLIS $mapdata @ $LOGADDR in $LOGCOMM / PID $LOGPID (Core $(( LOGCORE + 0 ))) "
+		ret=$LOGADDR
+		# try to resolve function
+		retrel=$(( RETADDR - LOGADDR + SYMOFFSET ))
+		if [[ $retrel -ge 0 ]] ; then
+			retrelhex=$(printf '%x\n' $retrel)
+			func=$(addr2line -e "${mapdata%%:*}" -f $retrelhex 2>/dev/null | head -n1 || true)
+			if [[ -n "$func" && "$func" != "??" ]] ; then
+				ret="$func (0x$retrelhex)"
+			fi
+		fi
+		if [[ "$line" != *" RETVAL="* ]] ; then
+			echo "called from $ret"
+		else
+			echo "return $RETVAL to $ret"
+		fi
+	done < <(cat $tracing/trace | sed -n -e 's/^[ ]*\([a-zA-Z0-9_-]\+\)-\([0-9]\+\)[ ]\+\[\([0-9]\+\)\][ ]\+....[ ]\+\([0-9]\+\).\([0-9]\+\): \([^:]\+\): (\([0-9a-fx <-]\+\))/LOGCOMM="\1" LOGPID=\2 LOGCORE="\3" LOGSEC=\4 LOGMILLIS=\5 LOGSYM="\6" LOGADDR="\7"/p')
 elif [[ "${1,,}" == "clear" ]] ; then
 	echo "Clear trace log..."
 	if ! echo > $tracing/trace ; then
@@ -49,7 +69,7 @@ elif [[ "${1,,}" == "stop" ]] ; then
 				echo "Unable to disable event '$line'" >&2
 			fi
 		fi
-	done < <(cat $tracing/uprobe_events | sed -e 's|^p:\([^/]\+\)/.*$|\1|' | sort -u)
+	done < <(cat $tracing/uprobe_events | sed -e 's|^[rp]:\([^/]\+\)/.*$|\1|' | sort -u)
 	if ! echo > $tracing/uprobe_events ; then
 		echo "Clearing uprobe events failed" >&2
 		status=1
@@ -74,17 +94,24 @@ else
 	symfiles+=( "/usr/lib/debug$path" )
 
 	# Get all symbols
+	baseaddr=$(readelf -W -l "$path" | sed -n -e 's/^[ ]*LOAD[ ]\+0x[0-9a-f]\+ \(0x[0-9a-f]\+\) 0x.*$/\1/p' | head -n1)
 	for symfile in $symfiles ; do
 		if [[ -f "$symfile" ]] ; then
 			while IFS= read -r line ; do
-				offset=${line%% *}
+				offset="0x${line%% *}"
 				sym=${line##* }
-				echo "$path:$sym (0x$offset)" >> $helpmap
-				if ! echo "p:${name}/${shortname}_$sym $path:0x$offset STACK=\$stack RET=\$stack0 MAPID=\\$(cat $helpmap | wc -l):u32" >> $tracing/uprobe_events ; then
-					echo "Adding ${name}/${shortname}_$sym ($path:0x$offset) failed" >&2
+				echo "$path:$sym ($offset)" >> $helpmap
+				addr=$(( offset - baseaddr ))
+				mapid=$(cat $helpmap | wc -l)
+				if ! echo "p:${name}/${shortname}_${sym} $path:$addr STACK=\$stack RETADDR=\$stack0 SYMOFFSET=\\${offset} MAPID=\\${mapid}:u32" >> $tracing/uprobe_events ; then
+					echo "Adding entry ${name}/${shortname}_${sym} @ $path:$addr ($offset - $baseaddr) failed" >&2
 					status=1
 				fi
-			done < <(objdump -tT "$symfile" | sed -n -e 's/\([0-9]*\) .*F \.text\t[0-9].* \([^ ]*\)$/\1 \2/p' | sort -n -s -k1,1 -u)
+				if ! echo "r:${name}/${shortname}_${sym}_ret $path:$addr STACK=\$stack SYMOFFSET=\\${offset} RETVAL=\$retval MAPID=\\${mapid}:u32" >> $tracing/uprobe_events ; then
+					echo "Adding return ${name}/${shortname}_${sym}_ret @ $path:$addr ($offset - $baseaddr) failed" >&2
+					status=1
+				fi
+			done < <(objdump -tT "$symfile" | sed -n -e 's/^[0]\+\([0-9a-f]\+\) .*F \.text\t[0-9a-f].* \([^ ]*\)$/\1 \2/p' | sort -n -s -k1,1 -u)
 		fi
 	done
 
@@ -127,7 +154,7 @@ else
 			fi
 		fi
 
-		# enable 
+		# enable
 		if ! echo 1 > $tracing/events/$name/enable ; then
 			echo "Enabling $name events failed" >&2
 			status=1
