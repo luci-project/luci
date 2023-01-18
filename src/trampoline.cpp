ce6f008ce491fa9c169914699809f5e2012631d5
Bernhard Heinloth <heinloth@cs.fau.de>
2023-01-18 18:10:15 +0100

    Improve trampoline: More secure and robust

--- src/trampoline.cpp
+++ src/trampoline.cpp
@@ -3,190 +3,155 @@
 #include <dlh/syscall.hpp>
 #include <dlh/assert.hpp>
 #include <dlh/log.hpp>
+#include <dlh/math.hpp>
 
 #include "object/identity.hpp"
 #include "object/base.hpp"
 #include "page.hpp"
 
-bool Trampoline::get_index(const VersionedSymbol & sym, size_t & index) const {
-	for (size_t i = 0; i < symbols.size(); i++) {
-		auto & other = symbols[i];
-		if (&(sym.object().file) == &(other.object().file) && (sym.name() == other.name() || strcmp(sym.name(), other.name()) == 0) && sym.version == other.version) {
-			index = i;
-			return true;
+const size_t trampoline_bytes = 16;  // Trampoline entry is 32 bytes
+const size_t entries_per_block = Page::SIZE / sizeof(uintptr_t);
+const size_t trampolines_block = Math::align_up(entries_per_block * trampoline_bytes, Page::SIZE);
+
+static void undefined_trampoline() {
+	LOG_ERROR << "Jumped to unassigned trampoline function!" << endl;
+	assert(false);
+}
+
+bool Trampoline::index(const VersionedSymbol & sym, size_t & index) const {
+	if (sym.type() != Elf::STT_FUNC && sym.type() != Elf::STT_GNU_IFUNC) {
+		LOG_WARNING << "Symbol trampoline can only be used for function types!" << endl;
+	} else {
+		for (size_t i = 0; i < symbols.size(); i++) {
+			if (symbols[i] == sym) {
+				index = i;
+				return true;
+			}
 		}
 	}
 	return false;
 }
 
-
-uintptr_t Trampoline::pointer(const VersionedSymbol & sym) const {
-	uintptr_t target = 0;
-	// Retrieve latest
-	if (auto resolved = sym.object().file.current->resolve_symbol(sym)) {
-		target = reinterpret_cast<uintptr_t>(resolved->pointer());
-	} else {
-		LOG_WARNING << "Symbol " << sym << " could not be found in (current) object!" << endl;
-	}
-	return target;
-}
-
-
-bool Trampoline::protect(uintptr_t page, bool writable) const {
-	if (page == 0)
-		return false;
-
-	// Protect mapping
-	int protection = PROT_READ | PROT_EXEC;
-	if (writable)
-		protection |= PROT_WRITE;
-
-	if (auto mprotect = Syscall::mprotect(page, Page::SIZE, protection)) {
+bool Trampoline::pointer(size_t index, void* & trampoline_function, uintptr_t* & target_address) const {
+	auto block = index / entries_per_block;
+	if (index < symbols.size() && block < blocks.size()) {
+		uintptr_t base = blocks[block];
+		size_t offset = index % entries_per_block;
+		trampoline_function = reinterpret_cast<void*>(base + offset * trampoline_bytes);
+		target_address = reinterpret_cast<uintptr_t*>(base + trampolines_block + offset * sizeof(uintptr_t));
 		return true;
-	} else {
-		LOG_ERROR << "Protecting page at " << (void*)page << " failed: " << mprotect.error_message() << endl;
-		return false;
 	}
+	return false;
 }
 
 
-void Trampoline::write_trampoline_code(void* addr, uintptr_t target) const {
-	uint8_t * a = reinterpret_cast<uint8_t*>(addr);
+bool Trampoline::allocate(const VersionedSymbol & sym, size_t & index) {
+	if ((symbols.size() + 1) / entries_per_block >= blocks.size()) {
+		// allocate new pages
+		if (auto mmap = Syscall::mmap(0, trampolines_block + Page::SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE)) {
+			uintptr_t memptr = mmap.value();
+			uint8_t * addr = reinterpret_cast<uint8_t*>(mmap.value());
+			uintptr_t * entry = reinterpret_cast<uintptr_t*>(mmap.value() + trampolines_block);
+			for (size_t i = 0; i < entries_per_block; i ++) {
+				// Set redirection pointer to undefined
+				entry[i] = reinterpret_cast<uintptr_t>(undefined_trampoline);
 
-	// endbr64
-	*(a++) = 0xf3;
-	*(a++) = 0x0f;
-	*(a++) = 0x1e;
-	*(a++) = 0xfa;
-	// movabs $target, %r11
-	*(a++) = 0x49;
-	*(a++) = 0xbb;
-	*(a++) = (target >> 0) & 0xff;
-	*(a++) = (target >> 8) & 0xff;
-	*(a++) = (target >> 16) & 0xff;
-	*(a++) = (target >> 24) & 0xff;
-	*(a++) = (target >> 32) & 0xff;
-	*(a++) = (target >> 40) & 0xff;
-	*(a++) = (target >> 48) & 0xff;
-	*(a++) = (target >> 56) & 0xff;
-	// jmpq *%r11
-	*(a++) = 0x41;
-	*(a++) = 0xff;
-	*(a++) = 0xe3;
-	// fill with nops
-	while (reinterpret_cast<uintptr_t>(a) % 16 != 0) {
-		*(a++) = 0x90;
-	}
+				// target is memory
+				uint32_t target = trampolines_block              // size of trampoline code
+					            + (sizeof(uintptr_t) * i)        // offset in redirection pointer memory
+					            - (trampoline_bytes * i + 0xa);  // offset in trampoline code (RIP relative)
 
-	assert(reinterpret_cast<uintptr_t>(a) - reinterpret_cast<uintptr_t>(addr) == size);
-}
+				// endbr64
+				*(addr++) = 0xf3;
+				*(addr++) = 0x0f;
+				*(addr++) = 0x1e;
+				*(addr++) = 0xfa;
 
+				// jmp *target(rip)
+				*(addr++) = 0xff;
+				*(addr++) = 0x25;
+				*(addr++) = (target >> 0) & 0xff;
+				*(addr++) = (target >> 8) & 0xff;
+				*(addr++) = (target >> 16) & 0xff;
+				*(addr++) = (target >> 24) & 0xff;
 
-void * Trampoline::get_trampoline(uintptr_t page, size_t index) const {
-	return reinterpret_cast<void*>(page + ((index * size) % Page::SIZE));
-}
-
-
-void * Trampoline::set_trampoline(uintptr_t page, size_t index) {
-	// get target
-	uintptr_t target = pointer(symbols[index]);
-	if (target == 0)
-		return nullptr;
-
-	// Write trampoline
-	void* addr = get_trampoline(page, index);
-	write_trampoline_code(addr, target);
-
-	// Pointer to trampoline
-	return addr;
-}
-
-
-void * Trampoline::get(size_t index) const {
-	assert(index < symbols.size());
-	size_t addrs_index = index / (Page::SIZE / size);
-	assert(addrs_index <= addrs.size());
-	return get_trampoline(addrs[addrs_index], index);
-}
-
-
-void * Trampoline::set(size_t index) {
-	assert(index < symbols.size());
-
-	size_t addrs_index = index / (Page::SIZE / size);
-	assert(addrs_index <= addrs.size());
-
-	uintptr_t page;
-	if (addrs_index == addrs.size()) {
-		// Map new page
-		if (auto mmap = Syscall::mmap(0, Page::SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE)) {
-			page = mmap.value();
-			addrs.push_back(page);
-			LOG_TRACE << "Got new page for trampoline at " << (void*)mmap.value() << endl;
+				// [6 byte] nop
+				*(addr++) = 0x66;
+				*(addr++) = 0x0f;
+				*(addr++) = 0x1f;
+				*(addr++) = 0x44;
+				*(addr++) = 0x00;
+				*(addr++) = 0x00;
+			}
+			if (auto mprotect = Syscall::mprotect(mmap.value(), trampolines_block, PROT_EXEC)) {
+				blocks.push_back(mmap.value());
+				LOG_TRACE << "Got new pages for trampoline at " << (void*)mmap.value() << endl;
+			} else {
+				LOG_ERROR << "Protecting page at " << (void*)mmap.value() << " failed: " << mprotect.error_message() << endl;
+				Syscall::munmap(mmap.value(), trampolines_block + Page::SIZE);
+				return false;
+			}
 		} else {
 			LOG_ERROR << "Requesting memory failed: " << mmap.error_message() << endl;
-			return nullptr;
+			return false;
 		}
-	} else {
-		page = addrs[addrs_index];
-		if (!protect(page, true))
-			return nullptr;
 	}
-
-	// create/overwrite trampoline
-	void * addr = set_trampoline(page, index);
-
-	// Protect mapping
-	protect(page, false);
-
-	return addr;
+	index = symbols.push_back(sym).index();
+	return true;
 }
 
-
 void * Trampoline::get(const VersionedSymbol & sym) const {
-	if (sym.type() != Elf::STT_FUNC && sym.type() != Elf::STT_GNU_IFUNC) {
-		LOG_WARNING << "Symbol trampoline can only be used for function types!" << endl;
-		return nullptr;
+	size_t idx = SIZE_MAX;
+	if (index(sym, idx)) {
+		uintptr_t * address = nullptr;
+		void * trampoline = nullptr;
+		if (pointer(idx, trampoline, address)) {
+			return trampoline;
+		}
 	}
-
-	size_t index = symbols.size();
-	if (get_index(sym, index))
-		return get(index);
-
 	return nullptr;
 }
 
-
 void * Trampoline::set(const VersionedSymbol & sym) {
-	if (sym.type() != Elf::STT_FUNC && sym.type() != Elf::STT_GNU_IFUNC) {
-		LOG_WARNING << "Symbol trampoline can only be used for function types!" << endl;
-		return nullptr;
+	// Retrieve latest
+	if (auto resolved = sym.object().file.current->resolve_symbol(sym)) {
+		size_t idx = SIZE_MAX;
+		if (index(sym, idx) || allocate(sym, idx)) {
+			uintptr_t * address = nullptr;
+			void * trampoline = nullptr;
+			if (pointer(idx, trampoline, address)) {
+				assert(address != nullptr);
+				*address = reinterpret_cast<uintptr_t>(resolved->pointer());
+				return reinterpret_cast<void*>(trampoline);
+			} else {
+				LOG_ERROR << "Unable to find trampoline pointer of " << idx << " for " << sym << endl;
+				assert(false);
+			}
+		}
+		LOG_ERROR << "Unable to find/create trampoline index for " << sym << endl;
+	} else {
+		LOG_ERROR << "Symbol " << sym << " could not be found in (current) object!" << endl;
 	}
-
-	size_t index = symbols.size();
-	if (!get_index(sym, index))
-		symbols.push_back(sym);
-
-	return set(index);
+	return nullptr;
 }
 
 
 void Trampoline::update() {
 	uintptr_t page = 0;
-	const size_t entries = Page::SIZE / size;
 	for (size_t i = 0; i < symbols.size(); i++) {
-		if (i % entries == 0) {
-			// protect old page
-			protect(page, false);
-			// get current page
-			size_t addrs_index = i / entries;
-			assert(addrs_index <= addrs.size());
-			page = addrs[addrs_index];
-			// unprotect current page
-			protect(page, true);
+		auto & sym = symbols[i];
+		if (auto resolved = sym.object().file.current->resolve_symbol(sym)) {
+			uintptr_t * address = nullptr;
+			void * trampoline = nullptr;
+			if (pointer(i, trampoline, address)) {
+				assert(address != nullptr);
+				*address = reinterpret_cast<uintptr_t>(resolved->pointer());
+			} else {
+				LOG_ERROR << "Unable to find trampoline pointer of " << i << " for " << sym << endl;
+				assert(false);
+			}
+		} else {
+			LOG_WARNING << "Symbol " << sym << " could not be found in (current) object on update!" << endl;
 		}
-		set_trampoline(page, i);
 	}
-	// protect old page
-	protect(page, false);
 }
--- src/trampoline.hpp
+++ src/trampoline.hpp
@@ -5,33 +5,17 @@
 #include "versioned_symbol.hpp"
 
 class Trampoline {
-	const size_t size = 32;  // Trampoline entry is 32 bytes
-	Vector<uintptr_t> addrs;
-	Vector<VersionedSymbol> symbols;
+	Vector<uintptr_t> blocks;  // a block starts with two code pages (similar to PLT) and an additional redirection table (GOT)
+	Vector<VersionedSymbol> symbols;  // list of assigned symbols, index corresponds to entry
 
 	/*! \brief Get vector index of symbol */
-	bool get_index(const VersionedSymbol & sym, size_t & index) const;
+	bool index(const VersionedSymbol & sym, size_t & index) const;
 
-	/*! \brief Retrieve address of symbol in current (!) object */
-	uintptr_t pointer(const VersionedSymbol & sym) const;
+	/*! \brief Retriefe pointer to trampoline code and target address for symbol at index */
+	bool pointer(size_t index, void* & trampoline_function, uintptr_t* & target_address) const;
 
-	/*! \brief Protected (non-writable) page at address */
-	bool protect(uintptr_t page, bool writable) const;
-
-	/*! \brief write trampoline to target at given address */
-	void write_trampoline_code(void * addr, uintptr_t target) const;
-
-	/*! \brief get corresponding address in page of trampoline code for symbol at index */
-	void * get_trampoline(uintptr_t page, size_t index) const;
-
-	/*! \brief put trampoline code for symbol at index in page to the corresponding position */
-	void * set_trampoline(uintptr_t page, size_t index);
-
-	/*! \brief Return trampoline function for symbol at index */
-	void * get(size_t index) const;
-
-	/*! \brief Generate and return trampoline code for symbol at index (including page handling) */
-	void * set(size_t index);
+	/*! \brief Allocate trampoline for symbol */
+	bool allocate(const VersionedSymbol & sym, size_t & index);
 
  public:
 	/*! \brief Return address of trampoline code for symbol (or nullptr if not found) */
