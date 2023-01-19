c8558c143efd59be528467bf2cf53e8f20fe02b0
Bernhard Heinloth <heinloth@cs.fau.de>
2023-01-19 10:42:46 +0100

    Remove old tests

--- test-old/loader/sys.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <time.h>
-
-int sys_nanosleep(const struct timespec *req, struct timespec *rem);
-int sys_write(int fd, const void *buf, size_t size);
-void sys_exit(int code);
